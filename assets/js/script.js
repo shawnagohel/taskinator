222da8793203e0d07f4845785edfb34d09b7c278
Shawna Gohel <shawnagohel@Shawnas-MacBook-Air.local>
2020-08-11 12:24:10 -0700

    type text for buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,29 +5,37 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
     event.preventDefault();
-  
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    console.log(taskTypeInput);
+    // create list item
+var listItemEl = document.createElement("li");
+listItemEl.className = "task-item";
+
+// create div to hold task info and add to list item
+var taskInfoEl = document.createElement("div");
+// give it a class name
+taskInfoEl.className = "task-info";
+// add HTML content to div
+taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+listItemEl.appendChild(taskInfoEl);
+
+// add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+
+    // var listItemEl = document.createElement("li");
+    // listItemEl.className = "task-item";
+
+    // listItemEl.textContent = taskNameInput;
+    // tasksToDoEl.appendChild(listItemEl);
+
 
   };
 
-// var createTaskHandler = function() {
-//     var listItemEl = document.createElement("li");
-//     listItemEl.className = "task-item";
-//     listItemEl.textContent = "This is a new task.";
-//     tasksToDoEl.appendChild(listItemEl);
-//   }
-  formEl.addEventListener("submit", createTaskHandler);
-//   buttonEl.addEventListener("click", createTaskHandler);
 
-// buttonEl.addEventListener("click", function() {
-//   var listItemEl = document.createElement("li");
-//   listItemEl.className = "task-item";
-//   listItemEl.textContent = "This is a new task.";
-//   tasksToDoEl.appendChild(listItemEl);
-// });
+  formEl.addEventListener("submit", createTaskHandler);
+
 
 
 
